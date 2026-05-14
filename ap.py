904c5b298566e3738e0a6a3a13d15c2b9ec3cca0
max1008eguilles <maxencemollard@hotmail.fr>
2026-05-14 09:19:05 +0200

    Update ap.py

--- ap.py
+++ ap.py
@@ -8,6 +8,7 @@ from datetime import datetime, date, timedelta
 from dateutil.relativedelta import relativedelta
 from streamlit_calendar import calendar
 
+
 # --- CONFIG DE LA PAGE ---
 st.set_page_config(page_title="RNM IMMO - Expert", layout="wide")
 
