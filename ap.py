c32cda79f63e585502c17573eb45048719cacb6c
max1008eguilles <maxencemollard@hotmail.fr>
2026-05-17 10:28:50 +0200

    Update ap.py

--- ap.py
+++ ap.py
@@ -7,6 +7,8 @@ import requests
 from datetime import datetime, date, timedelta
 from dateutil.relativedelta import relativedelta
 from streamlit_calendar import calendar
+import github  # N'oublie pas d'ajouter PyGithub dans ton fichier requirements.txt !
+from github import Github
 
 # --- CONFIG DE LA PAGE ---
 st.set_page_config(page_title="RNM IMMO - Expert", layout="wide")
